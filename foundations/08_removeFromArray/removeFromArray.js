95b4e0c3da78b8222e4cacccd91c8448132b251f
Chase Brown <chasebrown70131@gmail.com>
2026-03-15 14:14:11 -0500

    completed assignments

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -6,7 +6,10 @@ removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 
 See if you can make use of some built-in array methods in this exercise. */
 
-const removeFromArray = function () {};
+const removeFromArray = function (array, ...args) {
+  return array.filter((item) => !args.includes(item));
+};
 
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+// Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them:
 
+/*```javascript
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+``` */
+
+const sumAll = function (start, end) {
+  if (start > end) {
+    [start, end] = [end, start]; // this swaps the values so they are put in order
+  }
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i; // same as sum = sum + i
+  }
+  return sum;
 };
-
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
